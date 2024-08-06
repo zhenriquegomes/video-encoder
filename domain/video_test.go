020df7666534e0aa5074cbcf0e5cd333bd37b182
zhenriquegomes <henriquegomesnunes19@gmail.com>
2024-08-06 06:36:31 -0300

    test: adjust video tests

--- domain/video_test.go
+++ domain/video_test.go
@@ -2,39 +2,24 @@ package domain_test
 
 import (
 	"testing"
-	"time"
 
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/zhenriquegomes/video-encoder/domain"
 )
 
-func TestValidateIfVideoIsEmpty(t *testing.T) {
-	video := domain.NewVideo()
-	err := video.Validade()
-	assert.Error(t, err)
-}
-
 func TestVideoIDIsNotAnUUID(t *testing.T) {
-	video := domain.NewVideo()
+	video, err := domain.NewVideo("resource", "filepath")
+	assert.NotNil(t, video)
+	assert.Nil(t, err)
 
 	video.ID = "nouuid"
-	video.ResourceID = "resource"
-	video.FilePath = "filepath"
-	video.CreatedAt = time.Now()
-
-	err := video.Validade()
+	err = video.Validade()
 	assert.Error(t, err)
 }
 
 func TestVideoValidation(t *testing.T) {
-	video := domain.NewVideo()
+	video, err := domain.NewVideo("resource", "filepath")
 
-	video.ID = uuid.New().String()
-	video.ResourceID = "resource"
-	video.FilePath = "filepath"
-	video.CreatedAt = time.Now()
-
-	err := video.Validade()
+	assert.NotNil(t, video)
 	assert.Nil(t, err)
 }
